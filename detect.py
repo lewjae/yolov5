fd20bfe083a4aa06d97e50da3c230e5ab648fea4
lewjae <lewjae@gmail.com>
2021-04-08 15:23:02 -0700

    worked d435i

--- detect.py
+++ detect.py
@@ -60,6 +60,12 @@ def detect(save_img=False):
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
     for path, img, im0s, vid_cap in dataset:
+        """
+        print("Jae path: ", path)
+        print("Jae img shape: ",img.shape)
+        print("Jae im0s:", im0s)
+        print("Jae vid_cap: ", vid_cap)
+        """
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
