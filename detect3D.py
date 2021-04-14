0a25b2323c884299e5064af6386ce7dc8d830d0b
lewjae <lewjae@gmail.com>
2021-04-13 20:35:00 -0700

    changed font size

--- detect3D.py
+++ detect3D.py
@@ -234,7 +234,7 @@ def detect(save_img=False):
                         #print("Jae deproject:", rs.rs2_deproject_pixel_to_point(depth_intrin,[y,x],z))
                         #print("Jae-PC: ",pc[int(x),int(y),z])
                         label = f'{names[int(cls)]} {conf:.2f}, [{x:0.1f} {y:0.1f} {z:0.1f}]m'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
                         #print("JAE: xywh", xywh)
             # Print time (inference + NMS)
