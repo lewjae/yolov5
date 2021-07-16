92120073704efcfe3ef7fed3d247b084616242ba
mcwalmart21 <79066429+mcwalmart21@users.noreply.github.com>
2021-07-15 18:59:22 -0700

    Update detect_d435i_api.py

--- detect_d435i_api.py
+++ detect_d435i_api.py
@@ -1,12 +1,10 @@
+
 import argparse
 #from home.jlew.git.yolov5.helper_functions import convert_depth_pixel_to_metric_coordinate
 import time
 from pathlib import Path
-import sys
-from collections import defaultdict
 
 import cv2
-from flask.signals import request_started
 from numpy.core.fromnumeric import round_
 import torch
 import torch.backends.cudnn as cudnn
@@ -14,6 +12,8 @@ from numpy import random
 
 import pyrealsense2 as rs
 import numpy as np
+
+import threading 
 
 from models.experimental import attempt_load
 #from utils.datasets import LoadStreams, LoadImages
@@ -23,20 +23,29 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 # Import helper functions and classes written to wrap the RealSense, OpenCV and Kabsch Calibration usage
+from collections import defaultdict
 from realsense_device_manager import DeviceManager, post_process_depth_frame
-#from calibration_kabsch import PoseEstimation
-#from helper_functions import get_boundary_corners_2D, convert_depth_frame_to_pointcloud, get_clipped_pointcloud, convert_depth_pixel_to_metric_coordinate, get_masked_pointcloud, convert_pointcloud_to_depth
-#from measurement_task import calculate_boundingbox_points, new_visualise_measurements
+from calibration_kabsch import PoseEstimation
+from helper_functions import get_boundary_corners_2D, convert_depth_frame_to_pointcloud, get_clipped_pointcloud, convert_depth_pixel_to_metric_coordinate, get_masked_pointcloud, convert_pointcloud_to_depth
+from measurement_task import calculate_boundingbox_points, new_visualise_measurements
+
+
 
 # Import flask stuff and api
 from flask import Flask, request, jsonify
 import requests
 import json
 
-app = Flask(__name__)
+
+# Import for point cloud visulalization
+#from opencv_pointcloud_viewer import *
 
 class LoadRS:  # capture Realsense stream
+	thread = []
+	detected = {}
+
 	def __init__(self, pipe='rs', img_size=640, stride=32):
+		self.source = pipe
 		self.img_size = img_size
 		self.stride = stride
 		
@@ -82,6 +91,14 @@ class LoadRS:  # capture Realsense stream
 		#for calibration_info in intrinsics_devices:
 		for key, value in intrinsics_devices.items():
 			self.calibration_info_devices[key].append(value)
+		
+
+	def start(self):
+		print('creating detection thread')
+		self.thread = threading.Thread(target=self._thread)
+		print('starting detection thread')
+		self.thread.start()
+		print('Detection service initialization: DONE')
 
 	def __iter__(self):
 		self.count = -1
@@ -117,7 +134,7 @@ class LoadRS:  # capture Realsense stream
 			img0.append(color_image)
 			#Letter Box, BGR to RGB, to 3x416x416
 			#temp = letterbox(color_image, new_shape=self.img_size)[0]
-			img.append(letterbox(color_image, new_shape=self.img_size)[0][:,:,::-1].transpose(2,0,1))
+			img.append(self.letterbox(color_image, new_shape=self.img_size)[0][:,:,::-1].transpose(2,0,1))
 			depth_frames.append(filtered_depth_frame)
 			sources.append(device)
 
@@ -141,194 +158,206 @@ class LoadRS:  # capture Realsense stream
 	def __len__(self):
 		return 0
 
-# Aggregate for detected items from multiple cameras
-# Assuming that each camera has minimu false positives, take union of all detected items and compute maximum
-def aggregate(detected_items):
+	def _thread(self):
+		print("Starting thread!!")
+		self.detect()
 
-	items_dict = defaultdict(set)
-	for _, val in detected_items.items():
-		for  k, v in val.items():
-			items_dict[k].add(v)	
+	# Aggregate for detected items from multiple cameras
+	# Assuming that each camera has minimu false positives, take union of all detected items and compute maximum
+	def aggregate(self, detected_items):
 
-	agg_dict = {}
-	agg_dict = {k : max(items_dict[k]) for k in items_dict}
-	return agg_dict
+		items_dict = defaultdict(set)
+		for _, val in detected_items.items():
+			for  k, v in val.items():
+				items_dict[k].add(v)	
 
-# YoloV5 object detection inferencer
-@app.route('/detect', methods=['GET','POST'])
-def detect():
-	#source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-	source = "rs"
-	weights = "yolov5m_0502_best.pt"
-	view_img = True
-	imgsz = 640
-	webcam = True
-	depth_scale = 0.001  #D435i
-	augment = 'store_true'
-	conf_thres = 0.5 
-	iou_thres = 0.45 
-	classes = [0,1,2,3,4,5,6]
-	agnostic_nms = 'store_true'
-	device = ''
-	cut_off_distance = 1.1  #in meter. cuf-off distance for object detection
+		agg_dict = {}
+		agg_dict = {k : max(items_dict[k]) for k in items_dict}
+		return agg_dict
 
-	# Initialize
-	set_logging()
-	device = select_device(device)
-	half = device.type != 'cpu'  # half precision only supported on CUDA
+	# YoloV5 object detection inferencer
+	def detect(self):
+		print("Doing detect!!")
+		#source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+		source = self.source
+		weights = "yolov5m_0502_best.pt"
+		view_img = True
+		imgsz = self.img_size
+		webcam = True
+		depth_scale = 0.001  #D435i
+		augment = 'store_true'
+		conf_thres = 0.5 
+		iou_thres = 0.45 
+		classes = [0,1,2,3,4,5,6]
+		agnostic_nms = 'store_true'
+		device = ''
+		cut_off_distance = 1.1  #in meter. cuf-off distance for object detection
 
-	# Load model
-	model = attempt_load(weights, map_location=device)  # load FP32 model
-	imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
-	if half:
-		model.half()  # to FP16
+		# Initialize
+		set_logging()
+		device = select_device(device)
+		half = device.type != 'cpu'  # half precision only supported on CUDA
 
-	# Second-stage classifier
-	classify = False
-	if classify:
-		modelc = load_classifier(name='resnet101', n=2)  # initialize
-		modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+		# Load model
+		model = attempt_load(weights, map_location=device)  # load FP32 model
+		imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+		if half:
+			model.half()  # to FP16
 
-	# Set Dataloader
-	cudnn.benchmark = True  # set True to speed up constant image size inference
-	dataset = LoadRS(source, img_size=imgsz)
-
-	# Get names and colors
-	names = model.module.names if hasattr(model, 'module') else model.names
-	colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-
-	# Run inference
-	t0 = time.time()
-	img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
-	_ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
-	
-	# Initialize cv-detected item dictionary
-	detected = {}
-
-	for path, img, im0s, depth_frames, calibration_info_devices in dataset:
-
-		img = torch.from_numpy(img).to(device)
-		img = img.half() if half else img.float()  # uint8 to fp16/32
-		img /= 255.0  # 0 - 255 to 0.0 - 1.0
-		if img.ndimension() == 3:
-			img = img.unsqueeze(0)
-
-		# Inference
-		t1 = time_synchronized()
-		pred = model(img, augment=augment)[0]
-
-		# Apply NMS
-		#pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
-		pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
-		t2 = time_synchronized()
-
-		# Apply Classifier
+		# Second-stage classifier
+		classify = False
 		if classify:
-			pred = apply_classifier(pred, modelc, img, im0s)
+			modelc = load_classifier(name='resnet101', n=2)  # initialize
+			modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
 
-		#im0h = np.empty((480,640,3),int)
+		# Set Dataloader
+		cudnn.benchmark = True  # set True to speed up constant image size inference
+		#dataset = LoadRS(source, img_size=imgsz)
+		dataset = self
+
+		# Get names and colors
+		names = model.module.names if hasattr(model, 'module') else model.names
+		colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+		# Run inference
+		t0 = time.time()
+		img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+		_ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
 		
-		# Process detections
-		for i, det in enumerate(pred):  # detections per image
+		# Initialize cv-detected item dictionary
+		self.detected = {}
 
-			cam, s, im0, depth_image, frame = path[i], '%g: ' % i, im0s[i].copy(),depth_frames[i].copy(), dataset.count
+		for path, img, im0s, depth_frames, calibration_info_devices in dataset:
 
-			s += '%s '%cam
-			p = Path(cam)  # to Path
-			s += '%gx%g ' % img.shape[2:]  # print string
+			img = torch.from_numpy(img).to(device)
+			img = img.half() if half else img.float()  # uint8 to fp16/32
+			img /= 255.0  # 0 - 255 to 0.0 - 1.0
+			if img.ndimension() == 3:
+				img = img.unsqueeze(0)
 
-			# Initialize the cv-detected item dictionary
-			detected[cam] = {}
-			#detected[cam] = defaultdict(lambda: 0)
-			#print("R,t: ", cam, calibration_info_devices[cam][0].pose_mat)
-			gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-			if len(det):
-				# Rescale boxes from img_size to im0 size
-				det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-				#bbox = det[:, :4].cpu().numpy()
-				#print("Jae,  scaled det[:,:4] ", det[:,:4])
-				#print("\nJae - bbox ",bbox.shape, bbox)
-				
-				# Write results
-				#print("det: ",det)
-				for *xyxy, conf, cls in det:
+			# Inference
+			t1 = time_synchronized()
+			pred = model(img, augment=augment)[0]
 
-					if view_img:  # Add bbox to image
+			# Apply NMS
+			#pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+			pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
+			t2 = time_synchronized()
 
-						xyxy_torch = torch.tensor(xyxy).view(1, 4)
-						(xp,yp,wp,hp) = xyxy2xywh(xyxy_torch).view(-1).tolist()
+			# Apply Classifier
+			if classify:
+				pred = apply_classifier(pred, modelc, img, im0s)
 
-						# convert image pixel into meters
-						z = depth_image[int(yp),int(xp)]*depth_scale  # center
-						if z < cut_off_distance:
-							(x,y,z) = rs.rs2_deproject_pixel_to_point(calibration_info_devices[cam][0][rs.stream.color],[xp,yp],z)
-							#print("x,y,z inimag coord:   ", x,y,z)
-							#(xx,yy,zz) = convert_depth_pixel_to_metric_coordinate(z,xp,yp,calibration_info_devices[cam][0][rs.stream.color])
-						
-							#print("xx,yy,zz in imag coord: ", xx, yy, zz)	
-							#(xx,yy,zz) = rs.rs2_transform_point_to_point(calibration_info_devices[cam][2],[xx,yy,zz])
-							#print("xx,yy,zz after: ", xx, yy, zz)				
-							#label = f'{names[int(cls)]} {conf:.2f}'
-							label = f'{names[int(cls)]} {conf:.2f}, [{x:0.2f} {y:0.2f} {z:0.2f}]m'
-							plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
-							if names[int(cls)] in detected[cam]:
-								detected[cam][names[int(cls)]] += 1
+			#im0h = np.empty((480,640,3),int)
+			
+			# Process detections
+			for i, det in enumerate(pred):  # detections per image
+
+				cam, s, im0, depth_image, frame = path[i], '%g: ' % i, im0s[i].copy(),depth_frames[i].copy(), dataset.count
+
+				s += '%s '%cam
+				p = Path(cam)  # to Path
+				s += '%gx%g ' % img.shape[2:]  # print string
+
+				# Initialize the cv-detected item dictionary
+				self.detected[cam] = {}
+				#detected[cam] = defaultdict(lambda: 0)
+				#print("R,t: ", cam, calibration_info_devices[cam][0].pose_mat)
+				gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+				if len(det):
+					# Rescale boxes from img_size to im0 size
+					det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+					#bbox = det[:, :4].cpu().numpy()
+					#print("Jae,  scaled det[:,:4] ", det[:,:4])
+					#print("\nJae - bbox ",bbox.shape, bbox)
+					
+					# Write results
+					#print("det: ",det)
+					for *xyxy, conf, cls in det:
+
+						if view_img:  # Add bbox to image
+
+							xyxy_torch = torch.tensor(xyxy).view(1, 4)
+							(xp,yp,wp,hp) = xyxy2xywh(xyxy_torch).view(-1).tolist()
+
+							# convert image pixel into meters
+							z = depth_image[int(yp),int(xp)]*depth_scale  # center
+							if z < cut_off_distance:
+								(x,y,z) = rs.rs2_deproject_pixel_to_point(calibration_info_devices[cam][0][rs.stream.color],[xp,yp],z)
+								#print("x,y,z inimag coord:   ", x,y,z)
+								#(xx,yy,zz) = convert_depth_pixel_to_metric_coordinate(z,xp,yp,calibration_info_devices[cam][0][rs.stream.color])
+							
+								#print("xx,yy,zz in imag coord: ", xx, yy, zz)	
+								#(xx,yy,zz) = rs.rs2_transform_point_to_point(calibration_info_devices[cam][2],[xx,yy,zz])
+								#print("xx,yy,zz after: ", xx, yy, zz)				
+								#label = f'{names[int(cls)]} {conf:.2f}'
+								label = f'{names[int(cls)]} {conf:.2f}, [{x:0.2f} {y:0.2f} {z:0.2f}]m'
+								plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
+								if names[int(cls)] in self.detected[cam]:
+									self.detected[cam][names[int(cls)]] += 1
+								else:
+									self.detected[cam][names[int(cls)]] =1
 							else:
-								detected[cam][names[int(cls)]] =1
-						else:
-							print("item a far way: ",names[int(cls)], z)
+								print("item a far way: ",names[int(cls)], z)
 
-			# Print time (inference + NMS)
-			print(f'{s}Done. ({t2 - t1:.3f}s)')
+				# Print time (inference + NMS)
+				print(f'{s}Done. ({t2 - t1:.3f}s)')
 
-			# Stream results
-			cv2.namedWindow(str(i) + ": " + str(p), cv2.WINDOW_NORMAL)
-			cv2.imshow(str(i) + ": " + str(p), im0)
-		
-		print("Detected items: ", detected)
-		print("Aggregated: ", aggregate(detected))
-		print(" ")
+				# Stream results
+				cv2.namedWindow(str(i) + ": " + str(p), cv2.WINDOW_NORMAL)
+				cv2.imshow(str(i) + ": " + str(p), im0)
+			
+			print("Detected items: ", self.detected)
+			print("Aggregated: ", self.aggregate(self.detected))
+			print(" ")
 
-	print(f'Done. ({time.time() - t0:.3f}s)')
+		print(f'Done. ({time.time() - t0:.3f}s)')
 
-	with open('api_data_camera.json') as f:
-		data = json.load(f)
-	
-	rs_results =requests.post('http://localhost:8000/detect', json=data).json()
-	print("[Jae] api test")
 
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
-	# Resize and pad image while meeting stride-multiple constraints
-	shape = img.shape[:2]  # current shape [height, width]
+	def letterbox(self, img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+		# Resize and pad image while meeting stride-multiple constraints
+		shape = img.shape[:2]  # current shape [height, width]
 
-	if isinstance(new_shape, int):
-		new_shape = (new_shape, new_shape)
+		if isinstance(new_shape, int):
+			new_shape = (new_shape, new_shape)
 
-	# Scale ratio (new / old)
-	r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-	if not scaleup:  # only scale down, do not scale up (for better test mAP)
-		r = min(r, 1.0)
+		# Scale ratio (new / old)
+		r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+		if not scaleup:  # only scale down, do not scale up (for better test mAP)
+			r = min(r, 1.0)
 
-	# Compute padding
-	ratio = r, r  # width, height ratios
-	new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-	dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-	if auto:  # minimum rectangle
-		dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
-	elif scaleFill:  # stretch
-		dw, dh = 0.0, 0.0
-		new_unpad = (new_shape[1], new_shape[0])
-		ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+		# Compute padding
+		ratio = r, r  # width, height ratios
+		new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+		dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+		if auto:  # minimum rectangle
+			dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+		elif scaleFill:  # stretch
+			dw, dh = 0.0, 0.0
+			new_unpad = (new_shape[1], new_shape[0])
+			ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
-	dw /= 2  # divide padding into 2 sides
-	dh /= 2
+		dw /= 2  # divide padding into 2 sides
+		dh /= 2
 
-	if shape[::-1] != new_unpad:  # resize
-		img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-	top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-	left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-	img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-	return img, ratio, (dw, dh)
+		if shape[::-1] != new_unpad:  # resize
+			img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+		top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+		left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+		img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+		return img, ratio, (dw, dh)
+
+
+
+app = Flask(__name__)
+dataset = LoadRS('rs', 640)
+dataset.start()
+
+@app.route('/detect', methods=['GET'])
+def get_detected_items():
+	print('GET detect')
+	return str(dataset.detected)
+
 
 if __name__ == '__main__':
 	"""
@@ -360,5 +389,6 @@ if __name__ == '__main__':
 		else:
 			detect()
 	"""
-	detect()
+	app.run(host='0.0.0.0', debug=False, threaded=False)
+
 
