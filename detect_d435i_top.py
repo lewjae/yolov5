f7e3338824e9b468369d31913ae6ad3f51359400
lewjae <lewjae@gmail.com>
2021-07-16 13:06:55 -0700

    still not aligned

--- detect_d435i_top.py
+++ detect_d435i_top.py
@@ -105,8 +105,8 @@ class LoadRS:  # capture Realsense stream
         for (device, frame) in frames.items():
             #color_images[device] = np.asarray(frame[rs.stream.color].get_data())
             color_image = np.asarray(frame[rs.stream.color].get_data())
-            filtered_depth_frame = np.asarray(post_process_depth_frame(frame[rs.stream.depth], temporal_smooth_alpha=0.1, temporal_smooth_delta=80).get_data())
-            #filtered_depth_frame = np.asarray(frame[rs.stream.depth].get_data())
+            #filtered_depth_frame = np.asarray(post_process_depth_frame(frame[rs.stream.depth], temporal_smooth_alpha=0.1, temporal_smooth_delta=80).get_data())
+            filtered_depth_frame = np.asarray(frame[rs.stream.depth].get_data())
             #print("[Jae]: color_image",color_image.shape)
             img0.append(color_image)
             #Letter Box, BGR to RGB, to 3x416x416
@@ -165,7 +165,7 @@ def detect():
     cut_off_distance = 1.1  #in meter. cuf-off distance for object detection
 
     depth_scale = 0.001  #D435i
-    clipping_distance_in_meters = 0.75 #1.02 #1 meter
+    clipping_distance_in_meters = 0.98 #1.02 #1 meter
     clipping_distance = clipping_distance_in_meters / depth_scale
 
     # Initialize
@@ -278,7 +278,7 @@ def detect():
                                 xywh = xyxy2xywh(torch.tensor(xyxy).view(1, 4)).view(-1).tolist()
                                 # convert a list of float to a list of int
                                 xywh = list(map(int,xywh))
-                                #print("JAE: xywh", xywh)
+
                                 covered_img = cover_detected_items(covered_img, xywh, gray_color)
                                 depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channel
                                 bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), gray_color, covered_img)
@@ -352,34 +352,6 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
     return img, ratio, (dw, dh)
 
 if __name__ == '__main__':
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='rs', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    print(opt)
-    
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
-    """
+
     detect()
 
