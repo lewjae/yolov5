a36bf47e3d5bb0734190a193a9ca9dacce48ec0c
Steven Duval Ruilova <steven.duvalruilova@walmart.com>
2021-08-30 11:09:49 -0700

    refactor auth token helper function to include explicit flag

--- helpers/auth.py
+++ helpers/auth.py
@@ -1,6 +1,7 @@
 from functools import wraps
 import json
 import jwt
+import os
 
 from flask.globals import request
 from config import Config
@@ -13,6 +14,10 @@ def token_check():
         @wraps(f)
         def __token_check(*args, **kwargs):
             token = None
+
+            # check environment variable for secure gate
+            if not config.get('jwt_secure') == 'True':
+                return f(*args, **kwargs)
 
             # allow through any request from the same host (local)
             if(request.remote_addr == "127.0.0.1" or request.remote_addr == "0.0.0.0"):
